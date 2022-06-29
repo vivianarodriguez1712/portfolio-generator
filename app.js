70f026dead016bcf7dbc746ec17f4b86c97d403d
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-28 20:35:08 -0400

    saving

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+console.log(profileDataArgs);
 
-  console.log('================');
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+console.log(name, github);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
